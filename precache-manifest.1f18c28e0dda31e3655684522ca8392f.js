d93d4e97ff457829de0cb8b2ac67d4e02b53a973
Julia <riyamtechcampus@gmail.com>
2019-06-30 16:50:56 +0300

    Updates

---
+++ precache-manifest.1f18c28e0dda31e3655684522ca8392f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81280fbd78d19e711caa609a002ce08",
+    "url": "/Ultra-react-project/index.html"
+  },
+  {
+    "revision": "d1b5b2d4b67fec836754",
+    "url": "/Ultra-react-project/static/css/main.e41b2382.chunk.css"
+  },
+  {
+    "revision": "0bb205a1f3082edb18a1",
+    "url": "/Ultra-react-project/static/js/2.9e29e9b0.chunk.js"
+  },
+  {
+    "revision": "d1b5b2d4b67fec836754",
+    "url": "/Ultra-react-project/static/js/main.fc1f5571.chunk.js"
+  },
+  {
+    "revision": "a2648c5859013fbe833b",
+    "url": "/Ultra-react-project/static/js/runtime~main.3257837f.js"
+  }
+]);
